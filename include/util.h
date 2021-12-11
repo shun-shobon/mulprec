ced9680a53d8c516c42dc45669ec34b8d33f113f
Shuntaro Nishizawa <me@shun.technology>
2021-12-11 15:23:28 +0900

    refactor: Delete unnecessary util funcs

--- include/util.h
+++ include/util.h
@@ -2,11 +2,7 @@
 #define UTIL_H
 
 #include "mulprec.h"
-#include <stdio.h>
 
-extern int lprintf(FILE *, const char *, int, const char *, const char *, ...);
 extern void assert_ok(stat_t stat);
-
-#define debug(...) lprintf(stderr, __FILE__, __LINE__, __func__, __VA_ARGS__)
 
 #endif // UTIL_H
--- src/util.c
+++ src/util.c
@@ -1,29 +1,8 @@
 #include "util.h"
 
-#include <stdarg.h>
 #include <stdio.h>
 
 #include "mulprec.h"
-
-int lprintf(FILE *fp, const char *file, int line, const char *func,
-            const char *fmt, ...) {
-  flockfile(fp);
-
-  int n = 0;
-
-  n += fprintf(fp, "%s:%d:%s: ", file, line, func);
-
-  va_list ap;
-  va_start(ap, fmt);
-  n += vfprintf(fp, fmt, ap);
-  va_end(ap);
-
-  n += fprintf(fp, "\n");
-
-  funlockfile(fp);
-
-  return n;
-}
 
 void assert_ok(stat_t stat) {
   if (stat != STAT_OK) {
