82361b3113f3c78159298aa455f6a6c770143591
Shuntaro Nishizawa <me@shun.technology>
2021-12-11 16:00:23 +0900

    feat: Add `malloc_safe()`

--- include/util.h
+++ include/util.h
@@ -3,6 +3,9 @@
 
 #include "mulprec.h"
 
-extern void assert_ok(stat_t stat);
+extern void assert_ok(stat_t);
+extern void *malloc_safe_internal(size_t, const char *, int);
+
+#define malloc_safe(size) malloc_safe_internal(size, __FILE__, __LINE__)
 
 #endif // UTIL_H
--- src/util.c
+++ src/util.c
@@ -1,6 +1,7 @@
 #include "util.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "mulprec.h"
 
@@ -9,4 +10,15 @@ void assert_ok(stat_t stat) {
     printf("Error: %s\n", "error");
     exit(1);
   }
+}
+
+void *malloc_safe_internal(size_t size, const char *file, int line) {
+  void *ptr = malloc(size);
+  if (ptr == NULL) {
+    fprintf(stderr, "[Fatal]%s:%d: Memory allocation failed(%zu bytes)\n", file,
+            line, size);
+    abort();
+  }
+
+  return ptr;
 }
